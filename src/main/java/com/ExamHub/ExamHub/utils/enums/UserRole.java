5988f6f183cb20925aa24926ca4903f0a385ced8
Dinesh Reddy <dinesh.reddy@netsmartz.net>
2023-12-28 17:53:10 +0530

    added the whole authentication system with JWT login, forgot, change password

--- src/main/java/com/ExamHub/ExamHub/utils/enums/UserRole.java
+++ src/main/java/com/ExamHub/ExamHub/utils/enums/UserRole.java
@@ -3,6 +3,5 @@ package com.ExamHub.ExamHub.utils.enums;
 public enum UserRole {
     ADMIN,
     USER,
-
 }
 
---
+++ src/main/java/com/ExamHub/ExamHub/utils/helpers/PasswordUtils.java
@@ -0,0 +1,21 @@
+package com.ExamHub.ExamHub.utils.helpers;
+
+import org.passay.CharacterRule;
+import org.passay.EnglishCharacterData;
+import org.passay.PasswordGenerator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PasswordUtils {
+
+    public static String generateSecurePassword() {
+        CharacterRule LCR = new CharacterRule(EnglishCharacterData.LowerCase);
+        LCR.setNumberOfCharacters(5);
+        CharacterRule UCR = new CharacterRule(EnglishCharacterData.UpperCase);
+        UCR.setNumberOfCharacters(1);
+        CharacterRule DR = new CharacterRule(EnglishCharacterData.Digit);
+        DR.setNumberOfCharacters(1);
+        return new PasswordGenerator().generatePassword(7, LCR, UCR, DR);
+    }
+
+}
